9356b371e260ed858084ed2db7001404dd432868
user1 <user1@user1-PC>
2019-10-29 17:36:34 +0900

    10/29

--- 01_Array/src/com/kh/array/A_Array.java
+++ 01_Array/src/com/kh/array/A_Array.java
@@ -12,17 +12,52 @@ public class A_Array {
 		// int 형 변수 5개를 만들어 각각 값을 초기화 하고
 		// 출력하기
 		
-		int num1 =1;
-		int num2 =2;
-		int num3 =3;
-		int num4 =4;
-		int num5 =5;
+		//		int num1 =1;
+		//		int num2 =2;
+		//		int num3 =3;
+		//		int num4 =4;
+		//		int num5 =5;
+		//		
+		//		System.out.println(num1);
+		//		System.out.println(num2);
+		//		System.out.println(num3);
+		//		System.out.println(num4);
+		//		System.out.println(num5);
+	
+		// 배열 선언
+		// 자료형[] 배열명;
+		// 자료형 배열명[];
 		
-		System.out.println(num1);
-		System.out.println(num2);
-		System.out.println(num3);
-		System.out.println(num4);
-		System.out.println(num5);
+		int[] arr;
+		
+		// 배열 할당
+		// - 실제 값을 저장할 수 있는 배열공간 할당(== 생성)
+		// 배열명 = new 자료형[배열크기];
+		
+		
+		arr = new int[5];
+		
+		//		// 변수처럼 각 인덱스 요소에 접근 하여 값 저장 가능.
+		//		arr[0] = 1;
+		//		arr[1] = 2;
+		//		arr[2] = 3;
+		//		arr[3] = 4;
+		//		arr[4] = 5;
+				
+		for(int i=0; i<arr.length;i++) {
+			arr[i] = i+1;
+		}
+				// 변수처럼 각 배열요소에 접근하여 출력도 가능
+				
+		//		System.out.println("=== 배열 출력 ===");
+		//		System.out.println(arr[0]);
+		//		System.out.println(arr[1]);
+		//		System.out.println(arr[2]);
+		//		System.out.println(arr[3]);
+		//		System.out.println(arr[4]);
+		for(int i = 0; i < arr.length;i++) {
+			System.out.println(arr[i]);			
+		}
 		
 	}
 	
---
+++ 01_Array/src/com/kh/run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.run;
+
+import com.kh.array.A_Array;
+
+public class Run {
+
+	public static void main(String[] args) {
+		A_Array arr = new A_Array();
+		arr.method1();
+		
+		
+	}
+
+}
