d50b45d80053d7e216cb2d69fdfccfb42d790e81
user1 <user1@user1-PC>
2019-10-29 17:21:07 +0900

    array

---
+++ 01_Array/src/com/kh/array/A_Array.java
@@ -0,0 +1,29 @@
+package com.kh.array;
+
+public class A_Array {
+	
+	// 배열
+	// - 같은 자료형의 변수를 하나의 묶음으로 다루는 것.
+	// - 저장된 값마다 인덱스가 지정된다.
+	//   (index는 0부터 시작)
+	
+	public void method1() {
+		// 배열을 사용하지 않고 
+		// int 형 변수 5개를 만들어 각각 값을 초기화 하고
+		// 출력하기
+		
+		int num1 =1;
+		int num2 =2;
+		int num3 =3;
+		int num4 =4;
+		int num5 =5;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		
+	}
+	
+}
