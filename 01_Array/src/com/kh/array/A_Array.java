f2b15c314c72e267a789bd206d287f33ab74fa72
user1 <user1@user1-PC>
2019-10-30 10:21:31 +0900

    10/30

--- 01_Array/src/com/kh/array/A_Array.java
+++ 01_Array/src/com/kh/array/A_Array.java
@@ -79,6 +79,115 @@ public class A_Array {
 		
 	}
 	
+	public void method3() {
+
+	// 사과, 바나나, 오렌지, 파인애플, 키위
+	// 다섯개의 과일
+	// 인덱스를 입력받아 해당 인덱스에 저장된 과일을 출력.
+	
+		//String 배열 선언과 동시에 초기화
+		// 컴파일러가 알아서 배열의 크기를 할당하고, 각 요소에 값을 대입
+		
+		String[] fruit = {"사과", "바나나", "오렌지", "파인애플", "키위"};
+		
+		System.out.print("인덱스 입력 : ");
+		int index = sc.nextInt();
+		
+		if(index >= 0 && index < fruit.length ) {
+			System.out.println(fruit[index]);
+		}
+		
+	}
+	
+	public void method4() {
+		// 사과, 바나나, 오렌지, 파인애플, 키위
+		// 다섯 개의 과일이 저장되어있는 배열이 있다.
+		// 과일명을 입력받아 해당 과일이 몇번째 인덱스에 저장되어 있는지 출력
+		
+		String[] fruit = {"사과", "바나나", "오렌지", "파인애플", "키위"};
+		
+		System.out.print("과일명 입력 : ");
+		String str = sc.nextLine();
+		
+		boolean isTrue= true;		
+		for(int i =0; i < fruit.length ; i++) {
+				
+			if(fruit[i].equals(str)) {
+				// 현재 접근한 배열 요소의 값이
+				// 입력한 값과 같을 경우
+					System.out.println(i+"번째 인덱스에 있습니다.");
+					isTrue = false;
+					break; 
+				}
+		}
+		if (isTrue) {
+			System.out.println("입력한 값과 일치하는 과일이 없습니다.");
+		}
+		
+		
+		
+		
+		
+		
+	}
+	public void method5() {
+		// 문자열 3개를 저장할 수 있는 배열을 선언 및 할당하고
+		// 문자열 3개를 입력받아 배열에 순서대로 저장 후
+		// 검색할 문자열을 입력받아 배열에 몇번째 인덱스에 저장되어 있는지 출력 
+		
+		boolean isTrue= true;		
+		
+		String[] arr = new String[3];
+		String str ="";
+		//배열에 저장부
+		for(int i =0; i < arr.length ; i++) {
+			System.out.print("입력 : ");
+			str = sc.nextLine();
+				arr[i] = str;
+		}
+		
+		System.out.print("검색 입력 : ");
+		str = sc.nextLine();
+		
+		//배열 검색부
+		for(int i =0; i < arr.length ; i++) {
+			if(arr[i].equals(str)) {
+				// 현재 접근한 배열 요소의 값이
+				// 입력한 값과 같을 경우
+					System.out.println(i+"번째 인덱스에 있습니다.");
+					isTrue = false;
+					break; 
+				}
+		}
+		if (isTrue) {
+			System.out.println("입력한 값과 일치하는 과일이 없습니다.");
+		}
+		
+	}		
+	
+	
+	public void method6() {
+		
+		
+		double[] arr = new double[5];
+		double tall =0;
+		double avg =0;
+		
+		//배열에 저장부
+		for(int i =0; i < arr.length ; i++) {
+			System.out.print( i+" 번째 키 입력 : ");
+			tall = sc.nextDouble();
+				arr[i] = tall;
+				avg += tall;
+		}
+
+		avg /= arr.length;
+		
+		System.out.print("평균 키 : "+avg);
+		
+		}
+		
+	}
 	
 	
 	
@@ -93,8 +202,3 @@ public class A_Array {
 	
 	
 	
-	
-	
-	
-	
-}
--- 01_Array/src/com/kh/run/Run.java
+++ 01_Array/src/com/kh/run/Run.java
@@ -7,7 +7,9 @@ public class Run {
 	public static void main(String[] args) {
 		A_Array arr = new A_Array();
 //		arr.method1();
-		arr.method2();
+//		arr.method2();
+//		arr.method5();
+		arr.method6();
 		
 	}
 
