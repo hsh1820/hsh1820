2d227fcd0c9dcd00b83156686a8fe5ec322880c1
hansonghee <shhan6485@gmail.com>
2019-11-03 22:01:25 +0900

    11/3

---
+++ 07_ObjectArray_Homework_한송희/src/com/hw2/run/Run.java
@@ -0,0 +1,17 @@
+package com.hw2.run;
+
+import java.util.Scanner;
+
+import com.hw2.model.vo.Student;
+
+public class Run {
+public static void main(String[] args) {
+	Student[] st = new Student[10];
+	Scanner sc = new Scanner(System.in);
+	
+	while(true)	{
+		st[0] = new Student();
+		
+		}
+	}
+}
