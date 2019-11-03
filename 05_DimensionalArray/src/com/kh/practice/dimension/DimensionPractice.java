a97b5eb2dba36c30ae403b9d89be300ac69aab1d
hansonghee <shhan6485@gmail.com>
2019-11-03 20:45:23 +0900

    11/3

--- 05_DimensionalArray/src/com/kh/practice/dimension/DimensionPractice.java
+++ 05_DimensionalArray/src/com/kh/practice/dimension/DimensionPractice.java
@@ -1,5 +1,7 @@
 package com.kh.practice.dimension;
 
+import java.util.Scanner;
+
 public class DimensionPractice {
 	//	2차원 배열
 	//	실습 문제
@@ -79,6 +81,20 @@ public class DimensionPractice {
 	//	12 11 10 9
 	//	8 7 6 5
 	//	4 3 2 1 
+	public void practice3(){
+		int[][] arr = new int[4][4];
+		int count = 16;
+		
+		for(int i = 0; i<arr.length;i++) {
+			for(int j = 0 ; j<arr[i].length;j++) {
+				arr[i][j] = count;
+				count--;
+				System.out.print(arr[i][j]+ " ");
+			}System.out.println();
+		}
+	}
+	
+	
 	//	▶ 실습문제4
 	//	메소드 명 : public void practice4(){}
 	//	4행 4열 2차원 배열을 생성하여 0행 0열부터 2행 2열까지는 1~10까지의 임의의 정수 값 저장 후
@@ -93,6 +109,37 @@ public class DimensionPractice {
 	//	1행 값 값 값 1행 값들의 합
 	//	2행 값 값 값 2행 값들의 합
 	//	3행 0열 값들의 합 1열 값들의 합 2열 값들의 합 가로 세로 합
+	public void practice4(){
+		int[][] arr = new int[4][4];
+		int vhap=0;
+		int hhap=0;
+		int total=0;
+	
+		
+		for(int i =0; i<=arr.length/2;i++) {
+			for(int j=0; j<=arr[i].length/2;j++) {
+				arr[i][j]= (int)(Math.random()*10 +1);
+				vhap += arr[i][j];
+				System.out.printf("%3d", arr[i][j]);
+			}
+			System.out.printf("%3d\n",vhap);
+			total += vhap;
+			vhap=0;
+		}
+		for(int i =0; i<=arr.length/2;i++) {
+			for(int j=0; j<=arr[i].length/2;j++) {
+				hhap += arr[j][i];
+			}
+			System.out.printf("%3d", hhap);
+			total += hhap;
+			hhap =0;
+		}
+		System.out.printf("%3d", total);
+		
+	}
+	
+	
+	
 	//	▶ 실습문제5
 	//	메소드 명 : public void practice5(){}
 	//	2차원 배열의 행과 열의 크기를 사용자에게 직접 입력받되, 1~10사이 숫자가 아니면
@@ -107,9 +154,38 @@ public class DimensionPractice {
 	//	Q M B H
 	//	H B I X
 	//	G F X I 
-	//	1 2 3
-	//	4 5 6
-	//	7 8 9
+	public void practice5(){
+		Scanner sc = new Scanner(System.in);
+		int row = 0;
+		int col = 0;
+		
+		while(true) {
+			System.out.print("행 크기 : ");
+			row = sc.nextInt();
+			System.out.print("열 크기 : ");
+			col = sc.nextInt();
+			
+			if(!(row > 0 && row <11 && col >0 && col <11)) {
+				System.out.println("반드시 1~10 사이의 정수를 입력해야 합니다.");
+				continue;
+			}else {
+				break;
+			}
+		}//while end
+		
+		char[][] arr = new char[row][col];
+		for(int i =0 ;i<arr.length;i++) {
+			for(int j =0; j<arr[i].length;j++) {
+				arr[i][j]= (char)(Math.random()*10 + 65);
+				System.out.print(arr[i][j] + " ");
+			}System.out.println();
+		}
+		
+	}
+	
+	
+	
+	
 	//	▶ 실습문제6
 	//	메소드 명 : public void practice6(){}
 	//	String[][] strArr = new String[][] {{"이", "까", "왔", "앞", "힘"}, {"차", "지", "습", "으", "냅"}, {"원",
@@ -120,6 +196,18 @@ public class DimensionPractice {
 	//	2 5 8
 	//	3 6 9
 	//	기존 흐름 실습문제6 흐름
+	
+	public void practice6(){
+		String[][] strArr = new String[][] {{"이", "까", "왔", "앞", "힘"}, {"차", "지", "습", "으", "냅"}, {"원",
+				"열", "니", "로", "시"}, {"배", "심", "다", "좀", "다"}, {"열", "히", "! ", "더", "!! "}};
+		for(int i =0;i<strArr.length;i++) {
+			for(int j =0; j<strArr[i].length;j++)	{
+				System.out.print(strArr[j][i] + " ");
+			}System.out.println();
+		}
+	}
+	
+	
 	//	▶ 실습문제7
 	//	메소드 명 : public void practice7(){}
 	//	사용자에게 행의 크기를 입력 받고 그 수만큼의 반복을 통해 열의 크기도 받아
